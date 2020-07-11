e9a0d454d79cd87f365d258a9fddc978b3bdf315
Thomas26948 <thomas.nigoghossian@ensta-paris.fr>
2020-07-11 16:47:03 +0200

    added objectives in the main

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
 import requests
 import places
 import geocoding
+import distance
+
+#Objectives :
+#recuperation of all the infos (city, duration, activity prefered)
+#using geocoding to translate the city into lat / lng
+#using places.py to find corresponding activities
+#using distance to show informations about distances and time between each activity
+#return all of this info to the website
 
 
 def main():
-	
